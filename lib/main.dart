68230e536fae8596b283a2c1200857fddd78ff89
LAUDRY IZA FADIAN <skakmate87@gmail.com>
2021-12-14 16:03:16 +0700

    detele tes basic auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:bakul_sepatu/auth/login.dart';
-import 'package:bakul_sepatu/network/network.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   runApp(MyApp());
-  Api.getAuth('produk');
 }
 
 class MyApp extends StatelessWidget {
