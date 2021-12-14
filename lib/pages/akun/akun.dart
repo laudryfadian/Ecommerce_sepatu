6334bffc1030516639007c83fd01674278908c8f
LAUDRY IZA FADIAN <skakmate87@gmail.com>
2021-12-14 16:07:17 +0700

    update layout and add button logout

--- lib/pages/akun/akun.dart
+++ lib/pages/akun/akun.dart
@@ -1,4 +1,6 @@
+import 'package:bakul_sepatu/auth/login.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Akun extends StatefulWidget {
   @override
@@ -8,12 +10,25 @@ class Akun extends StatefulWidget {
 class _AkunState extends State<Akun> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Akun'),
-        centerTitle: true,
+    return Container(
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            SharedPreferences pref = await SharedPreferences.getInstance();
+            await pref.clear();
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => Login()),
+              (Route<dynamic> route) => false,
+            );
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.red.shade900),
+          child: Text(
+            "Logout",
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+        ),
       ),
-      backgroundColor: Colors.blueAccent,
     );
   }
 }
