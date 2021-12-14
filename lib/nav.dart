17d2e1508d9d28ce40a7287c5c0df014bbccf30b
LAUDRY IZA FADIAN <skakmate87@gmail.com>
2021-12-14 16:04:23 +0700

    change persitent nav bar to curved nav bar

--- lib/nav.dart
+++ lib/nav.dart
@@ -2,84 +2,93 @@ import 'package:bakul_sepatu/pages/akun/akun.dart';
 import 'package:bakul_sepatu/pages/home/home.dart';
 import 'package:bakul_sepatu/pages/keranjang/keranjang.dart';
 import 'package:bakul_sepatu/pages/produk/produk.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
-class Nav extends StatelessWidget {
+class Nav extends StatefulWidget {
   const Nav({Key? key}) : super(key: key);
 
-  List<Widget> _buildScreens() {
-    return [Home(), Produk(), Keranjang(), Akun()];
+  @override
+  _NavState createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  @override
+  void initState() {
+    super.initState();
   }
 
-  List<PersistentBottomNavBarItem> _navBarsItems() {
-    return [
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.home),
-        title: ("Home"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.cube_box),
-        title: ("Produk"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.cart),
-        title: ("Keranjang"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.person_crop_circle),
-        title: ("Akun"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-    ];
+  int _selectedIndex = 0;
+
+  // List<Widget> _buildScreens() {
+  //   return [Home(), Produk(), Keranjang(), Akun()];
+  // }
+  List<Widget> _widgetOptions = <Widget>[Home(), Produk(), Keranjang(), Akun()];
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
+
+  // List<PersistentBottomNavBarItem> _navBarsItems() {
+  //   return [
+  //     PersistentBottomNavBarItem(
+  //       icon: Icon(CupertinoIcons.home),
+  //       title: ("Home"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //     PersistentBottomNavBarItem(
+  //       icon: Icon(CupertinoIcons.cube_box),
+  //       title: ("Produk"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //     PersistentBottomNavBarItem(
+  //       icon: Icon(CupertinoIcons.cart),
+  //       title: ("Keranjang"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //     PersistentBottomNavBarItem(
+  //       icon: Icon(CupertinoIcons.person_crop_circle),
+  //       title: ("Akun"),
+  //       activeColorPrimary: CupertinoColors.activeBlue,
+  //       inactiveColorPrimary: CupertinoColors.systemGrey,
+  //     ),
+  //   ];
+  // }
 
   @override
   Widget build(BuildContext context) {
-    PersistentTabController _controller;
-
-    _controller = PersistentTabController(initialIndex: 0);
-
-    return PersistentTabView(
-      context,
-      controller: _controller,
-      screens: _buildScreens(),
-      items: _navBarsItems(),
-      confineInSafeArea: true,
-      backgroundColor: Colors.white, // Default is Colors.white.
-      handleAndroidBackButtonPress: true, // Default is true.
-      resizeToAvoidBottomInset:
-          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-      stateManagement: true, // Default is true.
-      hideNavigationBarWhenKeyboardShows:
-          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-      decoration: NavBarDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        colorBehindNavBar: Colors.white,
+    return Scaffold(
+      extendBody: true,
+      appBar: AppBar(
+        title: Text('Bakul Sepatu'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
       ),
-      popAllScreensOnTapOfSelectedTab: true,
-      popActionScreens: PopActionScreensType.all,
-      itemAnimationProperties: ItemAnimationProperties(
-        // Navigation Bar's items animation properties.
-        duration: Duration(milliseconds: 200),
-        curve: Curves.ease,
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      screenTransitionAnimation: ScreenTransitionAnimation(
-        // Screen transition animation on change of selected tab.
-        animateTabTransition: true,
-        curve: Curves.ease,
-        duration: Duration(milliseconds: 200),
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.blueAccent,
+        backgroundColor: Colors.red.withOpacity(0),
+        buttonBackgroundColor: Colors.blueAccent,
+        height: 50,
+        items: const <Widget>[
+          Icon(CupertinoIcons.home, color: Colors.white),
+          Icon(CupertinoIcons.cube_box, color: Colors.white),
+          Icon(CupertinoIcons.cart, color: Colors.white),
+          Icon(CupertinoIcons.person_crop_circle, color: Colors.white)
+        ],
+        animationDuration: Duration(milliseconds: 200),
+        animationCurve: Curves.bounceInOut,
+        index: _selectedIndex,
+        onTap: _onItemTap,
       ),
-      navBarStyle:
-          NavBarStyle.style9, // Choose the nav bar style with this property.
     );
   }
 }
