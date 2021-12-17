af2fe3340a56a97642ba415a6c28612e4fa7257e
LAUDRY IZA FADIAN <skakmate87@gmail.com>
2021-12-17 21:17:10 +0700

    update model of produk

--- lib/models/produk.dart
+++ lib/models/produk.dart
@@ -3,18 +3,27 @@ class Produks {
   final String nama;
   final String stok;
   final String hargaJual;
+  final String urlGambar;
+  final String size;
+  final String keterangan;
 
   Produks(
       {required this.id,
       required this.nama,
       required this.stok,
-      required this.hargaJual});
+      required this.hargaJual,
+      required this.urlGambar,
+      required this.size,
+      required this.keterangan});
 
   factory Produks.fromJson(Map<String, dynamic> json) {
     return Produks(
         id: json['_id'] as String,
         nama: json['nama'] as String,
         stok: json['stok'].toString(),
-        hargaJual: json['hargaJual'].toString());
+        hargaJual: json['hargaJual'].toString(),
+        urlGambar: json['urlGambar'] as String,
+        size: json['size'] as String,
+        keterangan: json['keterangan'] as String);
   }
 }
