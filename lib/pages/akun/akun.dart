d3fe477c5c0b7c7a386df98871f0d727c1894b2a
LAUDRY IZA FADIAN <skakmate87@gmail.com>
2021-12-17 21:18:00 +0700

    update layout page akun

--- lib/pages/akun/akun.dart
+++ lib/pages/akun/akun.dart
@@ -8,26 +8,100 @@ class Akun extends StatefulWidget {
 }
 
 class _AkunState extends State<Akun> {
+  String nama = "";
+  String email = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getCred();
+  }
+
+  void getCred() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      nama = pref.getString("nama")!;
+      email = pref.getString("email")!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            SharedPreferences pref = await SharedPreferences.getInstance();
-            await pref.clear();
-            Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (context) => Login()),
-              (Route<dynamic> route) => false,
-            );
-          },
-          style: ElevatedButton.styleFrom(primary: Colors.red.shade900),
-          child: Text(
-            "Logout",
-            style: TextStyle(color: Colors.white, fontSize: 20.0),
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            height: 50.0,
+            child: Row(
+              children: [
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Alesso_profile.png/467px-Alesso_profile.png'),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        nama,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25.0),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            // color: Colors.red,
+            child: Row(
+              children: [
+                Container(
+                  width: 100,
+                  // color: Colors.green,
+                  child: Text(
+                    'Email',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    email,
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                SharedPreferences pref = await SharedPreferences.getInstance();
+                await pref.clear();
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => Login()),
+                  (Route<dynamic> route) => false,
+                );
+              },
+              style: ElevatedButton.styleFrom(primary: Colors.red.shade900),
+              child: Text(
+                "Logout",
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
